d61d9fe4b63103c161de3ef6893976ad0946be89
literaryverses <98240549+literaryverses@users.noreply.github.com>
2024-12-22 16:26:30 -0700

    add background filter

---
+++ background_filter.gd
@@ -0,0 +1,19 @@
+extends CanvasModulate
+
+@onready var player = $'../Player'
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if player:
+		match player.lens:
+			player.LENS_COLOR.RED:
+				color = Color('RED')
+			player.LENS_COLOR.GREEN:
+				color = Color('GREEN')
+			player.LENS_COLOR.BLUE:
+				color = Color('BLUE')
