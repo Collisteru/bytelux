3ee385827676dbeb8c0f76c31fb54c67ffe99e73
Sean Carter <collisteru@gmail.com>
2024-12-22 16:59:45 -0700

    Working on laser

--- entities/player_laser/player_laser.gd
+++ entities/player_laser/player_laser.gd
@@ -2,7 +2,29 @@ extends RayCast2D
 
 func fire_laser(laser_position, player_position):
 	
-	print("Laser fired at ", laser_position, "From player at ", player_position)
+	print("laser_position:  ", laser_position)
+	print("Player Position: ", player_position)
+		
+	# Calculate angle
+	var x_diff = laser_position.x - player_position.x
+	var y_diff = laser_position.y - player_position.y
+	
+	var angle;
+	
+	# X increases left to right, y increases top to bottom
+	# We need to resolve the angle for each quadrant to make the trig work
+	
+	
+	# Quadrant I (top-right, pos x_diff, neg y_diff)
+	print("X_diff: ", x_diff, "Y diff: ", y_diff)
+	if (x_diff >= 0 and y_diff < 0):
+		angle = atan(y_diff / x_diff)
+	# Quadrant II (top-left, neg x_diff, neg y_diff
+	elif (x_diff < 0 and y_diff < 0):
+		print("Quad 2!")
+		angle = atan(y_diff / x_diff)
+	
+	print("Laser fired from ", player_position, " at angle ", angle)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
--- entities/test_player/test_player.gd
+++ entities/test_player/test_player.gd
@@ -6,14 +6,36 @@ var lens = LENS_COLOR.WHITE
 
 # Import child nodes
 @onready var laser = $PlayerLaser
+@onready var sprite = $PlayerSprite
 
+func translate_to_center(position: Vector2) -> Vector2:
+		# Get the size of the viewport
+		var screen_size = get_viewport().get_visible_rect().size
+		# Calculate the center of the screen
+		var screen_center = screen_size / 2
+		# Translate the position to be relative to the center
+		return position - screen_center
 
 
 func _input(event: InputEvent) -> void:
 	# Get player's response to mouse events
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		var player_position = self.position
-		laser.fire_laser(event.position, player_position)
+		# By default, the click position is done with reference to the screen. We want it to be done with reference to the player position
+
+		# Get positions
+		# Get the player's global position
+		var global_position = self.to_global(Vector2.ZERO)
+
+		# Convert the global position to screen space
+		var player_pos = self.get_global_transform_with_canvas()
+		var player_position = player_pos.get_origin()
+		
+		var click_position: Vector2;
+		# Get position w/ ref to player
+		click_position.x = event.position.x - player_position.x
+		click_position.y = event.position.y + 30 - player_position.y
+		
+		laser.fire_laser(click_position, player_position)
 		
 	# Get player's response to key events
 	if event is InputEventKey and event.pressed:
