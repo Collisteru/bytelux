a61fc3dd3ac4379789a96c4a9473754336ceded5
literaryverses <98240549+literaryverses@users.noreply.github.com>
2024-12-22 04:45:50 -0700

    lens selection from player

--- entities/test_player/test_player.gd
+++ entities/test_player/test_player.gd
@@ -1,13 +1,27 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
+enum LENS_COLOR {RED, BLUE, GREEN, WHITE}
+var lens = LENS_COLOR.WHITE
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey and event.pressed:
+		match event.keycode:
+			KEY_1:
+				print("Player pressed 1")
+				self.lens = LENS_COLOR.RED
+			KEY_2:
+				print("Player pressed 2")
+				self.lens = LENS_COLOR.BLUE
+			KEY_3:
+				print("Player pressed 3")
+				self.lens = LENS_COLOR.GREEN
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction for left/right and up/down
 	var direction_x := Input.get_axis("ui_left", "ui_right")
 	var direction_y := Input.get_axis("ui_up", "ui_down")
-	
+
 	# Handle horizontal (x) movement
 	if direction_x != 0:
 		velocity.x = direction_x * SPEED
