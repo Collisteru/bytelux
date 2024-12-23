351305774e05a96c1eca082fbc34ab3725bca034
Sean Carter <collisteru@gmail.com>
2024-12-22 17:18:13 -0700

    Firing angle has been resolved

--- entities/player_laser/player_laser.gd
+++ entities/player_laser/player_laser.gd
@@ -13,16 +13,21 @@ func fire_laser(laser_position, player_position):
 	# X increases left to right, y increases top to bottom
 	# We need to resolve the angle for each quadrant to make the trig work
 	
-	
 	# Quadrant I (top-right, pos x_diff, neg y_diff)
 	print("X_diff: ", x_diff, "Y diff: ", y_diff)
 	if (x_diff >= 0 and y_diff < 0):
-		angle = atan(y_diff / x_diff)
+		angle = atan(y_diff / x_diff) + 2 * PI
 	# Quadrant II (top-left, neg x_diff, neg y_diff
 	elif (x_diff < 0 and y_diff < 0):
 		print("Quad 2!")
+		angle = atan(y_diff / x_diff) + PI
+	# Quadrant III (bottom-left, neg x_diff, pos y_diff)
+	elif (x_diff < 0 and y_diff >= 0):
+		angle = PI + atan(y_diff / x_diff)
+	# Quadrant IV (bottom-right, pos x_diff, pos y_diff)
+	elif (x_diff >= 0 and y_diff >= 0):
 		angle = atan(y_diff / x_diff)
-	
+
 	print("Laser fired from ", player_position, " at angle ", angle)
 
 # Called when the node enters the scene tree for the first time.
