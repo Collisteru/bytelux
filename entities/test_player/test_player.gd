45fca22c9751379f0de0be3d9be8c6fc1aff1af3
Sean Carter <collisteru@gmail.com>
2024-12-23 16:30:40 -0700

    Die function finished, fixed bug

--- entities/test_player/test_player.gd
+++ entities/test_player/test_player.gd
@@ -4,6 +4,8 @@ const SPEED = 300.0
 const ACCELERATION = 30.0
 enum LENS_COLOR {RED, BLUE, GREEN, WHITE}
 var lens = LENS_COLOR.RED
+var player_is_alive
+
 
 # Import child nodes
 @onready var laser = $PlayerLaser
@@ -11,6 +13,10 @@ var lens = LENS_COLOR.RED
 @onready var pointer = $Pointer
 @onready var body = $BodySprite
 @onready var player_camera = $Camera2D
+
+func _ready():
+	# Set player living/death flag
+	player_is_alive = true
 
 func translate_to_center(position: Vector2) -> Vector2:
 		# Get the size of the viewport
@@ -22,53 +28,58 @@ func translate_to_center(position: Vector2) -> Vector2:
 
 
 func _input(event: InputEvent) -> void:
-	# Get player's response to mouse events
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		# By default, the click position is done with reference to the screen. We want it to be done with reference to the player position
+	
+	if player_is_alive:
+		# Get player's response to mouse events
+		if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			# By default, the click position is done with reference to the screen. We want it to be done with reference to the player position
 
-		# Get positions
-		# Get the player's global position
-		var global_position = self.to_global(Vector2.ZERO)
+			# Get positions
+			# Get the player's global position
+			var global_position = self.to_global(Vector2.ZERO)
 
-		# Convert the global position to screen space to get player position with reference to camera
-		var global_player_position = self.position
-		
-		# Get player position with ref to scene
-		var camera_player_position = self.get_global_transform_with_canvas().get_origin()
-		
-		var click_position: Vector2 = get_local_mouse_position();
-		
-		var screensize = get_viewport().size 
-		
-		# Get position w/ ref to player
-		#click_position.x = (event.position.x - camera_player_position.x)#/screensize.x
-		#click_position.y = (event.position.y - camera_player_position.y)#/screensize.y
-		 		
-		laser.fire_laser(click_position, camera_player_position, self)
-		
-		#pointer.move(click_position, global_position)
-		
-		print("\n position: ", self.position)
-		#print("\n calc'd click: ", click_position)
-		#print("\n camera_player_position: ", camera_player_position)
-		#print("\n click: ", get_viewport().get_mouse_position())
-		
-	# Get player's response to key events
-	if event is InputEventKey and event.pressed:
-		match event.keycode:
-			KEY_1:
-				self.lens = LENS_COLOR.RED
-			KEY_2:
-				self.lens = LENS_COLOR.BLUE
-			KEY_3:
-				self.lens = LENS_COLOR.GREEN
-				RenderingServer.set_default_clear_color('GREEN')
-			KEY_K:
-				# Kill self (debugging purposes)
-				# TODO: Remove
-				self.die(player_camera)
-				
+			# Convert the global position to screen space to get player position with reference to camera
+			var global_player_position = self.position
+			
+			# Get player position with ref to scene
+			var camera_player_position = self.get_global_transform_with_canvas().get_origin()
+			
+			var click_position: Vector2 = get_local_mouse_position();
+			
+			var screensize = get_viewport().size 
+			
+			# Get position w/ ref to player
+			#click_position.x = (event.position.x - camera_player_position.x)#/screensize.x
+			#click_position.y = (event.position.y - camera_player_position.y)#/screensize.y
+			 		
+			laser.fire_laser(click_position, camera_player_position, self)
+			
+			#pointer.move(click_position, global_position)
+			
+			print("\n position: ", self.position)
+			#print("\n calc'd click: ", click_position)
+			#print("\n camera_player_position: ", camera_player_position)
+			#print("\n click: ", get_viewport().get_mouse_position())
+			
+		# Get player's response to key events
+		if event is InputEventKey and event.pressed:
+			match event.keycode:
+				KEY_1:
+					self.lens = LENS_COLOR.RED
+				KEY_2:
+					self.lens = LENS_COLOR.BLUE
+				KEY_3:
+					self.lens = LENS_COLOR.GREEN
+					RenderingServer.set_default_clear_color('GREEN')
+				KEY_K:
+					# Kill self (debugging purposes)
+					# TODO: Remove
+					self.die(player_camera)
+					
 func die(camera) -> void:
+	
+	# Set player_is_alive flag to false, making it impossible to perform player actions
+	player_is_alive = false
 	
 	# Duplicate camera
 	# (the player camera is a child of the original class, so we need to create a new one with the same properites to 
@@ -80,11 +91,19 @@ func die(camera) -> void:
 	new_camera.zoom = camera.zoom
 	new_camera.offset = camera.offset
 	get_parent().add_child(new_camera)
+	
+	# Switch to this camera
+	new_camera.make_current()
 
 	# Spawn playerdeathparticles
 	
 	# Instance the particle scene
 	var particle_scene = preload("res://entities/particles/player_explosion_node.tscn").instantiate()
+	
+
+	
+	# Make player invisible.
+	self.visible = false
 	
 	# Assign position of the particles to be the same as the player
 	particle_scene.position = self.position
@@ -93,8 +112,8 @@ func die(camera) -> void:
 	get_parent().add_child(particle_scene)
 	
 	# Clear player from scene
-	queue_free()
-	
+	#queue_free()
+	#
 	# Wait for a time equal to the duration of the particle effect then 
 
 
