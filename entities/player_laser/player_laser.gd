a3e7feefada346dea8efc8020fefdc7427e86026
Sean Carter <collisteru@gmail.com>
2024-12-22 17:52:42 -0700

    Laser direction done; no collision

--- entities/player_laser/player_laser.gd
+++ entities/player_laser/player_laser.gd
@@ -1,7 +1,15 @@
 extends RayCast2D
 
+@onready var laser_line = $LaserLine2D
+
+# TODO: This is a placeholder until wall collision code is programmed
+var laser_max_length = 1000  # Max length of the laser
+
+func _ready():
+	laser_line.visible = false  # Initially hidden
+
+
 func fire_laser(laser_position, player_position):
-	#
 	#print("laser_position:  ", laser_position)
 	#print("Player Position: ", player_position)
 	# Calculate angle
@@ -12,28 +20,24 @@ func fire_laser(laser_position, player_position):
 	
 	# X increases left to right, y increases top to bottom
 	# We need to resolve the angle for each quadrant to make the trig work
+
+	# Create laser from that angle
 	
-	# Quadrant I (top-right, pos x_diff, neg y_diff)
-	print("X_diff: ", x_diff, "Y diff: ", y_diff)
-	if (x_diff >= 0 and y_diff < 0):
-		angle = atan(y_diff / x_diff) + 2 * PI
-	# Quadrant II (top-left, neg x_diff, neg y_diff
-	elif (x_diff < 0 and y_diff < 0):
-		print("Quad 2!")
-		angle = atan(y_diff / x_diff) + PI
-	# Quadrant III (bottom-left, neg x_diff, pos y_diff)
-	elif (x_diff < 0 and y_diff >= 0):
-		angle = PI + atan(y_diff / x_diff)
-	# Quadrant IV (bottom-right, pos x_diff, pos y_diff)
-	elif (x_diff >= 0 and y_diff >= 0):
-		angle = atan(y_diff / x_diff)
+	var vec_norm = sqrt(pow(x_diff,2) + pow(y_diff, 2));
+	var dir_norm = Vector2(x_diff / vec_norm, y_diff / vec_norm)
+	var laser_end: Vector2 = (laser_max_length * dir_norm)
+	
+	print("Laser end: ", laser_end)
+	
+	laser_line.points = [Vector2(0, -10), laser_end]
 
-	print("Laser fired from ", player_position, " at angle ", angle)
+	laser_line.visible = true  # Show the laser line
+	
+	fade()
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+func fade():
+	await get_tree().create_timer(0.05).timeout
+	laser_line.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
