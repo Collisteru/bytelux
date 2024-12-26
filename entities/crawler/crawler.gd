b1748e5274a9856bbd2406e445662a9970cee4b1
Wyndolin Crook <wyncrook@protonmail.com>
2024-12-26 12:35:27 -0700

    oops forogr

--- entities/crawler/crawler.gd
+++ entities/crawler/crawler.gd
@@ -3,7 +3,6 @@ extends "res://entities/enemy_base/enemy_base.gd"
 #@onready var hitbox = $Hitbox
 #@onready hitbox.connect
 @onready var projectile_scene = load("res://entities/projectile/projectile.tscn")
-@onready var sprite = $"AnimatedSprite2D"
 
 func _ready() -> void:
 	var targetColor = LensColor.translate_color(myColor)
