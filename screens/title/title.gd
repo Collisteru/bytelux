bef0d6c1b2d9ed86c34d82ee6ff8906876e8e9a1
literaryverses <98240549+literaryverses@users.noreply.github.com>
2024-12-26 00:08:05 -0700

    assigning level 1 to screen

--- screens/title/title.gd
+++ screens/title/title.gd
@@ -98,9 +98,7 @@ func _on_exit_pressed() -> void:
 
 
 func _on_start_pressed() -> void:
-	#TODO move to tutorial
-	get_tree().change_scene_to_file("res://levels/vic_level_design_for_funsies.tscn")
-	pass
+	get_tree().change_scene_to_file("res://levels/Level1.tscn")
 
 
 func _on_settings_pressed() -> void:
