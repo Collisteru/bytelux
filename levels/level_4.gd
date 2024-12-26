ebc5b1ad042498a0a0649d85211bd397af761eac
literaryverses <98240549+literaryverses@users.noreply.github.com>
2024-12-26 12:31:25 -0700

    making level layout

--- levels/level_4.gd
+++ levels/level_4.gd
@@ -1,10 +1,8 @@
-extends Node2D
-
 @onready var door = $Door
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:	
-	door.set_next_level("res://levels/Level4.tscn")
+	door.set_next_level("res://levels/Level5.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
