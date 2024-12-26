f4df7a546511be0d890799a55ac5f308da535ded
literaryverses <98240549+literaryverses@users.noreply.github.com>
2024-12-26 03:13:50 -0700

    redirect player death to lose screen

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -263,7 +263,7 @@ func die(camera = player_camera) -> void:
 	
 	
 	
-	get_tree().change_scene_to_file("res://screens/title/title.tscn")
+	get_tree().change_scene_to_file("res://screens/lose/lose.tscn")
 
 func _physics_process(_delta: float) -> void:
 	# Get the input direction
