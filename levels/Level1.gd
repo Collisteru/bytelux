f43e4f02338f1c9beb53af6381879bf84549e14c
Sean Carter <collisteru@gmail.com>
2024-12-26 18:27:01 -0700

    Finished (more or less) static animation

--- levels/Level1.gd
+++ levels/Level1.gd
@@ -7,6 +7,7 @@ extends Node2D
 func _ready() -> void:
 	LensColor.change_lens(LensColor.LENS_COLOR.WHITE)
 	door.set_next_level("res://levels/vic_level_design_for_funsies.tscn")
+	$Background/StaticSprite/StaticAnim.play("static")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
