8f088c48fce8541b45c2215d77b17b694ec04ed5
Sean Carter <collisteru@gmail.com>
2024-12-26 19:08:53 -0700

    Added static background. Animation does not yet play in all levels

--- levels/level_2.gd
+++ levels/level_2.gd
@@ -6,6 +6,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:	
 	LensColor.change_lens(LensColor.LENS_COLOR.RED)
+	$Background/StaticSprite/StaticAnim.play("static")
 	door.set_next_level("res://levels/Level3.tscn")
 	
 
