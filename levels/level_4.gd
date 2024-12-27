9fd24ebbb940b5c0657eb22d4694721e5fae44c9
Sean Carter <collisteru@gmail.com>
2024-12-26 20:43:29 -0700

    Added static backgrounds to levels

--- levels/level_4.gd
+++ levels/level_4.gd
@@ -6,6 +6,8 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	LensColor.change_lens(LensColor.LENS_COLOR.RED)
+	$Background/StaticSprite/StaticAnim.play("static")
+
 	door.set_next_level("res://levels/Level5.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
