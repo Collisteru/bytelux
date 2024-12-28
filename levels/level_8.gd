8ef554533d20eecef0f95a75a4e473a33b5ec267
Sean Carter <collisteru@gmail.com>
2024-12-27 21:09:29 -0700

    Workin on Level10

--- levels/level_8.gd
+++ levels/level_8.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	LensColor.change_lens(LensColor.LENS_COLOR.RED)
+	LensColor.change_lens(LensColor.LENS_COLOR.GREEN)
 	$Background/StaticSprite/StaticAnim.play("static")
 
 	door.set_next_level("res://levels/Level9.tscn")
