b3077911a47426b78aff4bb22c547d59df337669
Sean Carter <collisteru@gmail.com>
2024-12-28 14:40:42 -0700

    itchio page

--- levels/level_8_5.gd
+++ levels/level_8_5.gd
@@ -6,7 +6,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	LensColor.change_lens(LensColor.LENS_COLOR.RED)
-	$StaticSprite/StaticAnim.play("static")
+	$Background/StaticSprite/StaticAnim.play("static")
 	#$Background/StaticSprite/StaticAnim.play("static")
 	door.set_next_level("res://levels/Level9.tscn")
 	LastLevelUpdater.set_last_level("res://levels/Level8_5.tscn")
