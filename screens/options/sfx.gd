73e442c61651c6695d6e8fc8bd99b09a59c2e580
literaryverses <98240549+literaryverses@users.noreply.github.com>
2025-05-22 11:24:09 -0600

    cleanup

--- screens/options/sfx.gd
+++ screens/options/sfx.gd
@@ -1,13 +1,5 @@
 extends VSlider
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# TODO: Make it so that these are set by the SFX and Music volume globals
 	self.value = 50
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
