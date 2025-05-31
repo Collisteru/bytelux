6e9c687bd075776ed2a5e6fcaa6d775ba787e528
Sean Carter <collisteru@gmail.com>
2025-05-31 17:30:42 -0600

    Changed to Bytelux

--- screens/title/title.gd
+++ screens/title/title.gd
@@ -19,7 +19,7 @@ func _ready() -> void:
 	rows = int(viewport_size.y / FONT_SIZE)-8
 	
 	# set window title
-	DisplayServer.window_set_title("COMPUTATRUM")
+	DisplayServer.window_set_title("BYTELUX")
 
 	# load font
 	font_resource = load("res://font/CARBON-DROID.ttf")
